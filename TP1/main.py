339ceefb383c74b62d7f3b1853c2ac7491c86e5e
AfroMangue <46705790+AfroMangue@users.noreply.github.com>
2023-10-27 15:33:08 +0200

    TP1

--- TP1/main.py
+++ TP1/main.py
@@ -1,14 +1,18 @@
-# main.py
+# Importation des classes Quiz et Question à partir des modules externes.
 from quiz_module import Quiz
 from question import Question
 
+# Définition de la fonction principale du programme.
 def main():
+    # Messages de bienvenue pour les utilisateurs.
     print("Bienvenue au Quiz IoT !")
     print("Testez vos connaissances en IoT avec ce quiz.")
     print("Vous avez 10 questions à répondre. Bonne chance!\n")
 
+    # Création d'une instance de la classe Quiz pour gérer le quiz.
     quiz = Quiz()
 
+    # Définition des données des questions sous forme de liste de tuples.
     questions_data = [
         ("Qu'est-ce que l'acronyme IoT signifie?", ["Internet of Things", "Internet of Time", "Internet of Technology"], "a"),
         ("Quelle technologie sans fil est couramment utilisée pour la communication IoT?", ["LoRaWAN", "Zigbee", "Bluetooth"], "a"),
@@ -22,12 +26,15 @@ def main():
         ("Quelle entreprise a popularisé le terme 'IoT'?", ["Apple", "Cisco", "Kevin Ashton"], "c"),
     ]
 
+    # Boucle pour créer des objets Question à partir des données et les ajouter au quiz.
     for data in questions_data:
         question = Question(data[0], data[1], data[2])
         quiz.ajouter_question(question)
 
+    # Début du quiz en appelant la méthode commencer_quiz() et affichage du score à la fin.
     quiz.commencer_quiz()
     quiz.afficher_score()
 
+# Vérification si le script est exécuté en tant que programme autonome.
 if __name__ == "__main__":
-    main()
+    main()  # Appel de la fonction main() pour lancer le quiz.
